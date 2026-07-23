8bc4f3ff80e81c98fbd3b36e627a4a9bfeecc45a
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2026-07-23 22:37:17 +0000

    Updated site title and OG metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'After the Story';
-export const SITE_DESCRIPTION = 'Interviews with legendary characters about their lives after the final chapter.';
+export const SITE_TITLE = 'Hamilton Labs AI SEO Gen App';
+export const SITE_DESCRIPTION = 'AI SEO Image Generator .';
